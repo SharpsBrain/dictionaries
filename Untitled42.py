55496ad45e580e047ff6ab8ca3706fd3da8ab814
SharpsBrain <109470818+SharpsBrain@users.noreply.github.com>
2022-09-16 21:52:51 +0500

    Add files via upload

---
+++ Untitled42.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[17]:
+
+
+thisdic= {
+    "brand": "ford",
+    "model": False,
+    "year":1954,
+    "colours":["red", "white", "blue"]
+}
+print(len(thisdic))
+print(thisdic["brand"])
+print(thisdic["model"])
+print(thisdic["year"])
+print(thisdic["colours"])
+print(thisdic)
+print(type(thisdic))
+
+
+# In[ ]:
+
+
+
+
